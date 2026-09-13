5778c724cbf3a01f744d5c67ceafffd11aef29b4
Oviya-Babu <ob1710@srmist.edu.in>
2026-09-13 13:51:53 +0530

    arrays

---
+++ Arrays/SumOfArrays.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class SumOfArrays{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int res = sum(arr);
+        System.out.print(res);
+        sc.close(); 
+}
+    public static int sum(int[] arr){
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++){
+            sum += arr[i];
+        }
+        return sum;
+    }
+    
+    
+}
