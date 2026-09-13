ba71d981fa3b2a1cc51ce162eca2c750005863b6
Oviya-Babu <ob1710@srmist.edu.in>
2026-09-13 17:18:02 +0530

    oddno in the array

---
+++ Arrays/OddNo.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class OddNo{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int res = OddArraynum(arr);
+        System.out.print(res);
+        sc.close();
+        
+    }
+    public static int OddArraynum(int[] arr){
+        int count = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] % 2 != 0){
+                count++;
+            }
+        }
+        return count;
+    }
+}
